b2cf33e8830b8573c55918ab1e1cc88fa7f8dbae
TóthBarnabásBéla(Szf <TthBarnabsBla(Szf@Tanulo-227-10>
2025-11-13 13:53:21 +0100

    Controller alapszerkezet

--- src/controller/JatekTerController.java
+++ src/controller/JatekTerController.java
@@ -1,5 +1,47 @@
 package controller;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JButton;
+import model.JatekTerModel;
+import view.Gui;
+
 public class JatekTerController {
-    
+    private final JatekTerModel model;
+    private final Gui nezet;
+    private final JButton[] gombok = new JButton[9];
+
+    public JatekTerController(JatekTerModel model, Gui nezet) {
+        this.model = model;
+        this.nezet = nezet;
+        hozzarendeles();
+        frissitTeljesNezet();
+        nezet.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                kilepes();
+            }
+        });
+    }
+
+    private void hozzarendeles() {
+
+    }
+
+    private void frissitTeljesNezet() {
+
+    }
+
+    private void mentes() {
+
+    }
+
+    private void betoltes() {
+
+    }
+
+    private void kilepes() {
+        nezet.kilepes();
+    }
+
 }
