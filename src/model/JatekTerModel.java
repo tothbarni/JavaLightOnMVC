3f9e885774226521b5c6764c2a2aa9074ea7787c
TóthBarnabásBéla(Szf <TthBarnabsBla(Szf@Tanulo-227-10>
2025-11-13 13:45:18 +0100

    Model szerkezet mukodessel

--- src/model/JatekTerModel.java
+++ src/model/JatekTerModel.java
@@ -29,22 +29,38 @@ public class JatekTerModel {
     }
 
     public void kapcsol(int index) {
-
+        int sor = index / 3;
+        int oszlop = index % 3;
+        valt(index);
+        if (oszlop > 0) valt(index - 1);
+        if (oszlop < 2) valt(index + 1);
+        if (sor > 0) valt(index - 3);
+        if (sor < 2) valt(index + 3);
+        lepesSzam++;
     }
 
     private void valt(int index) {
-        
+        mezoAllapot[index] = !mezoAllapot[index];
     }
 
     public void alapKezdes() {
-        
+        for (int i = 0; i < mezoAllapot.length; i++) {
+            mezoAllapot[i] = false;
+        }
+        lepesSzam = 0;
     }
 
-    public void randomKezdes() {
-        
+    public void randomKezdes(java.util.Random r) {
+        for (int i = 0; i < mezoAllapot.length; i++) {
+            mezoAllapot[i] = r.nextBoolean();
+        }
+        lepesSzam = 0;
     }
 
     public boolean nyert() {
+        for (boolean b : mezoAllapot) {
+            if (!b) return false;
+        }
         return true;
     }
 }
