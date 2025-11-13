038829ffda7ef3a6f11fcfb63dd5ff6d178dfc20
TóthBarnabásBéla(Szf <TthBarnabsBla(Szf@Tanulo-227-10>
2025-11-13 14:46:52 +0100

    JatekTerModelTest alapszerkezet

---
+++ test/model/JatekTerModelTest.java
@@ -0,0 +1,31 @@
+package model;
+
+public class JatekTerModelTest {
+
+    public static void main(String[] args) {
+        JatekTerModelTest teszt = new JatekTerModelTest();
+
+    }
+
+    private final JatekTerModel model = new JatekTerModel();
+
+    private void alapKezdesMindenMezoKikapcsolva() {
+
+    }
+
+    private void kapcsolKeresztetKapcsolAt() {
+
+    }
+
+    private void kapcsolSaroknalCsakLehetsegesSzomszedokValtanak() {
+
+    }
+
+    private void getMezoAllapotVisszaadottTombModositasaNemHatasos() {
+
+    }
+
+    private void nyertCsakAkkorIgazHaMindenBeVanKapcsolva() {
+
+    }
+}
